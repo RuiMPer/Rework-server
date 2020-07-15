95ba1df64ecc68e939af9237d9599cf679133b83
Margarida Pascoal <margaridapascoal@Margaridas-MBP.lan>
2020-07-15 20:22:16 +0100

    auth route tiny changes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -110,27 +110,27 @@ authRoutes.post('/login', (req, res, next) => {
     //http://www.passportjs.org/docs/configure/
 
     passport.authenticate('local', (err, theUser, failureDetails) => {
-        if (err) {
-            res.status(500).json({ message: 'Something went wrong authenticating user' });
-            return;
-        }
-        if (!theUser) {
-            // "failureDetails" contains the error messages
-            // from our logic in "LocalStrategy" { message: '...' }.
-            res.status(401).json(failureDetails);
-            return;
-        }
-        // save user in session
-        req.login(theUser, (err) => {
             if (err) {
-                res.status(500).json({ message: 'Session save went bad.' });
+                res.status(500).json({ message: 'Something went wrong authenticating user' });
                 return;
             }
-            // We are now logged in (that's why we can also send req.user)
-            res.status(200).json(theUser);
-        });
-    })(req, res, next);
-});
+            if (!theUser) {
+                // "failureDetails" contains the error messages
+                // from our logic in "LocalStrategy" { message: '...' }.
+                res.status(401).json(failureDetails);
+                return;
+            }
+            // save user in session
+            req.login(theUser, (err) => {
+                if (err) {
+                    res.status(500).json({ message: 'Session save went bad.' });
+                    return;
+                }
+                // We are now logged in (that's why we can also send req.user)
+                res.status(200).json(theUser);
+            });
+        })(req, res, next);
+    });
 
 authRoutes.post('/logout', (req, res, next) => {
     // req.logout() is defined by passport
