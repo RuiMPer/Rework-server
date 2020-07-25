547a6f3e36b7676b46fe72f646a16b952e7f01cd
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-07-25 11:34:22 +0100

    heroku fix

--- app.js
+++ app.js
@@ -22,11 +22,12 @@ require('./configs/passport');
 // IF YOU STILL DIDN'T, GO TO 'configs/passport.js' AND UN-COMMENT OUT THE WHOLE FILE
 
 mongoose
-  // .connect(process.env.MONGODB_URI, {
-  //   useCreateIndex: true,
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true})
-  .connect('mongodb://localhost/rework-server', { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  // .connect('mongodb://localhost/rework-server', { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
