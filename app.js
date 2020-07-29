d23c8dda9b9152d02f6152d720e75b6015c0c813
RuiMPer <rui.miguel.pereira.rp@gmail.com>
2020-07-29 20:08:26 +0100

    aws fix5

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    // credentials: true,
+    credentials: true,
     origin: [`${process.env.FRONTEND_URL}`, "http://finalproject-rework-app-ironhack.s3-website-eu-west-1.amazonaws.com"]
   })
 )
